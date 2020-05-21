e3c353a47354e3dd3f122d31c11cc114b0469d95
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-05-21 23:11:22 +0400

    added PhoneNumberValidator.js

---
+++ PhoneNumberValidator.js
@@ -0,0 +1,55 @@
+// Must return true for a certain set of 
+// valid US phone number strings
+//555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+
+// Conditional solution tailored to problem
+// Regex solution is a shorter alternative
+const telephoneCheck = (str) => {
+
+    let cleanStr = str.replace(/[\D]/g, '');
+    let digitCount = cleanStr.length;
+
+    if (digitCount < 10 || digitCount > 11) {
+        return false;
+    }
+
+    if (str.match(/[^\d-()\s]/gm)) {
+        return false;
+    }
+
+    let noCountryCodeNum = str.replace(/\s/gm, '');
+
+    if (digitCount === 11 && str[0] === "1") {
+        noCountryCodeNum = noCountryCodeNum.slice(1);
+    } else if (digitCount !== 10) {
+        return false
+    }
+
+    let parenCount = noCountryCodeNum.match(/[()]/gm);
+    let dashCount = noCountryCodeNum.match(/[-]/);
+
+    if (parenCount) {
+        if (parenCount.length > 2 || parenCount.length === 1) {
+            return false
+        }
+    }   if (noCountryCodeNum.indexOf(')') > 4) {
+            return false
+    }
+
+    if (dashCount) {
+        if (dashCount.length > 2) {
+        return false
+        }
+    }
+    
+    return true;
+  }
+
+console.log( telephoneCheck("1 555-555-5555"));  
+console.log( telephoneCheck("6054756961"));
+console.log( telephoneCheck("1 (555) 555-5555"));
